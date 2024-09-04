1447367e483739a4db5560128916a20694363ef8
utkarshdagoat <utkarsh382004@gmail.com>
2024-09-05 02:18:23 +0530

    adds staking runtime outline

--- packages/chain/src/runtime/modules/staking.ts
+++ packages/chain/src/runtime/modules/staking.ts
@@ -10,11 +10,38 @@ export class StakingRegistry extends RuntimeModule<StakingRegistryConfig> {
     @state() public stakes = StateMap.from<PublicKey, UInt64>(PublicKey, UInt64);
     @state() public hasStaked = StateMap.from<PublicKey, Bool>(PublicKey, Bool);
     @state() public isSlashed = StateMap.from<PublicKey, Bool>(PublicKey, Bool);
-    @state() public slashTreasury = PublicKey;
+    @state() public slashTreasury = State.from<PublicKey>(PublicKey);
+
+    constructor (_slashTreasury: PublicKey) {
+        super();
+        this.slashTreasury.set(_slashTreasury); ;
+    }
 
     @runtimeMethod()
     public async stake() {
         //TODO: check msg.value
     }
 
+    @runtimeMethod()
+    public async unstake(){
+        //TODO: check msg.value
+    }
+
+    @runtimeMethod()
+    public async slash(){
+
+    }
+
+    public async hasStakedAddress(address: PublicKey): Promise<Bool> {
+        return (await this.hasStaked.get(address)).value;
+    }
+
+    public async getStakedAmount(address: PublicKey): Promise<UInt64> {
+        return (await this.stakes.get(address)).value;
+    }
+
+
+    public async isNotSlashed(address: PublicKey): Promise<Bool> {
+        return (await this.isSlashed.get(address)).value.not();
+    }
 }
