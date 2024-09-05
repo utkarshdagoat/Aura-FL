dcdac486ff51e3aed752f07db8f9ac2b026e860a
utkarshdagoat <utkarsh382004@gmail.com>
2024-09-05 21:03:35 +0530

    adds publish test modules

--- packages/chain/src/runtime/modules/publish.ts
+++ packages/chain/src/runtime/modules/publish.ts
@@ -4,6 +4,7 @@ import { runtimeMethod, RuntimeModule, runtimeModule, state } from "@proto-kit/m
 import { assert, State, StateMap } from "@proto-kit/protocol";
 import { inject } from "tsyringe";
 import { StakingRegistry } from "./staking";
+import { Balance, Balances } from "@proto-kit/library";
 
 
 /**
@@ -20,7 +21,17 @@ import { StakingRegistry } from "./staking";
  * @param feePerEpoch - The fee per epoch
  */
 export class Task extends Struct({
-    name: Bytes(32),
+  epochs: UInt64,
+  modelType: UInt64,
+  modelSize: UInt64,
+  numOfLayers: UInt64,
+  activationFunction: UInt64,
+  Optimizer: UInt64,
+  completed: Bool,
+  publisher: PublicKey,
+  feePerEpoch: UInt64
+}) {
+  public static from(
     epochs: UInt64,
     modelType: UInt64,
     modelSize: UInt64,
@@ -30,113 +41,114 @@ export class Task extends Struct({
     completed: Bool,
     publisher: PublicKey,
     feePerEpoch: UInt64
-}) {
-    public static from(
-        name: Bytes,
-        epochs: UInt64,
-        modelType: UInt64,
-        modelSize: UInt64,
-        numOfLayers: UInt64,
-        activationFunction: UInt64,
-        Optimizer: UInt64,
-        completed: Bool,
-        publisher: PublicKey,
-        feePerEpoch: UInt64
-    ): Task {
-        return new Task({
-            name,
-            epochs,
-            modelType,
-            modelSize,
-            numOfLayers,
-            activationFunction,
-            Optimizer,
-            completed,
-            publisher,
-            feePerEpoch
-        });
+  ): Task {
+    return new Task({
+      epochs,
+      modelType,
+      modelSize,
+      numOfLayers,
+      activationFunction,
+      Optimizer,
+      completed,
+      publisher,
+      feePerEpoch
+    });
+  }
+
+  public toJSON() {
+    return {
+      epochs: this.epochs.toJSON(),
+      modelType: this.modelType.toJSON(),
+      modelSize: this.modelSize.toJSON(),
+      numOfLayers: this.numOfLayers.toJSON(),
+      activationFunction: this.activationFunction.toJSON(),
+      Optimizer: this.Optimizer.toJSON(),
+      completed: this.completed.toJSON(),
+      publisher: this.publisher.toJSON(),
+      feePerEpoch: this.feePerEpoch.toJSON()
     }
+  }
+
 };
 
 type PublisherConfig = Record<string, never>;
 
 @runtimeModule()
 export class Publisher extends RuntimeModule<PublisherConfig> {
-    @state() public TASKS_LENGTH = State.from<UInt32>(UInt32);
-    @state() public tasks = StateMap.from<UInt32, Task>(UInt32, Task);
-    @state() public clients = StateMap.from<UInt32, Array<PublicKey>>(UInt32, Provable.Array(PublicKey, 3));
+  @state() public TASKS_LENGTH = State.from<UInt32>(UInt32);
+  @state() public tasks = StateMap.from<UInt32, Task>(UInt32, Task);
+  @state() public clients = StateMap.from<UInt32, Array<PublicKey>>(UInt32, Provable.Array(PublicKey, 3));
 
-    // constructor(@inject("StakingRegistry") public stakingRegistry: StakingRegistry) {
-    //     super();
-    // }
+  constructor(
+    @inject("StakingRegistry") public stakingRegistry: StakingRegistry,
+    @inject("Balances") public balances: Balances
+  ) {
+    super();
+  }
 
-    @runtimeMethod()
-    public async addTask(
-        name: Bytes,
-        epochs: UInt64,
-        modelType: UInt64,
-        modelSize: UInt64,
-        numOfLayers: UInt64,
-        activationFunction: UInt64,
-        Optimizer: UInt64,
-        feePerEpoch: UInt64
-    ): Promise<void> {
-        //TODO: Check feePerEpoch*epochs >= message.value 
-        const tasksLength = await this.TASKS_LENGTH.get();
-        const task = Task.from(
-            name,
-            epochs,
-            modelType,
-            modelSize,
-            numOfLayers,
-            activationFunction,
-            Optimizer,
-            Bool.fromValue(false),
-            this.transaction.sender.value,
-            feePerEpoch
-        );
-        await this.tasks.set(tasksLength.value, task);
-        this.TASKS_LENGTH.set(tasksLength.value.add(UInt32.from(1)));
-    }
+  @runtimeMethod()
+  public async addTask(
+    epochs: UInt64,
+    modelType: UInt64,
+    modelSize: UInt64,
+    numOfLayers: UInt64,
+    activationFunction: UInt64,
+    Optimizer: UInt64,
+    feePerEpoch: UInt64
+  ): Promise<void> {
+    //TODO: Check feePerEpoch*epochs >= message.value 
+    const tasksLength = await this.TASKS_LENGTH.get();
+    const task = Task.from(
+      epochs,
+      modelType,
+      modelSize,
+      numOfLayers,
+      activationFunction,
+      Optimizer,
+      Bool.fromValue(false),
+      this.transaction.sender.value,
+      feePerEpoch
+    );
+    await this.tasks.set(tasksLength.value, task);
+    this.TASKS_LENGTH.set(tasksLength.value.add(UInt32.from(1)));
+  }
 
-    @runtimeMethod()
-    public async completeTask(taskId: UInt32): Promise<void> {
-        const sender = this.transaction.sender.value;
-        const task = await this.tasks.get(taskId);
+  @runtimeMethod()
+  public async completeTask(taskId: UInt32): Promise<void> {
+    const sender = this.transaction.sender.value;
+    const task = await this.tasks.get(taskId);
 
-        assert(task.value.publisher.equals(sender), "Only the publisher can complete the task");
+    assert(task.value.publisher.equals(sender), "Only the publisher can complete the task");
 
-        task.value.completed = Bool.fromValue(true);
-        await this.tasks.set(taskId, task.value);
-        await this._distributeFunds(taskId);
-    }
+    task.value.completed = Bool.fromValue(true);
+    await this.tasks.set(taskId, task.value);
+    await this._distributeFunds(taskId);
+  }
 
-    async _distributeFunds(taskId: UInt32) {
-        const task = await this.tasks.get(taskId);
-        const clients = (await this.clients.get(taskId)).value;
-        const feePerEpoch = task.value.feePerEpoch;
-        const epochs = task.value.epochs;
-        const totalFee = feePerEpoch.mul(epochs);
-        const feePerClient = totalFee.div(UInt64.from(clients.length));
-        // for (let i = 0; i < clients.length; i++) {
-        //     await runtime.balances.addBalance(clients[i], feePerClient);
-        // }
-        //TODO: Add the fee to the clients
-    }
+  async _distributeFunds(taskId: UInt32) {
+    const task = await this.tasks.get(taskId);
+    const clients = (await this.clients.get(taskId)).value;
+    const feePerEpoch = task.value.feePerEpoch;
+    const epochs = task.value.epochs;
+    const totalFee = feePerEpoch.mul(epochs);
+    const feePerClient = totalFee.div(UInt64.from(clients.length));
 
-    @runtimeMethod()
-    public async addClient(client: PublicKey, taskId: UInt32): Promise<void> {
-        // const hasClienStaked = await this.stakingRegistry.hasStakedAddress(client);
-        // assert(hasClienStaked, "Client has not staked");
-        const clients = (await this.clients.get(taskId)).value;
-        assert(Bool.fromValue(clients.length < 3), "Only 3 clients can be added to a task");
-        clients.push(client);
-        await this.clients.set(taskId, clients);
-    }
+    //TODO: Add the fee to the clients
+  }
 
-    @runtimeMethod()
-    public async getTask(taskId: UInt32): Promise<Task> {
-        return (await this.tasks.get(taskId)).value;
-    }
+  @runtimeMethod()
+  public async addClient(client: PublicKey, taskId: UInt32): Promise<void> {
+    // const hasClienStaked = await this.stakingRegistry.hasStakedAddress(client);
+    // assert(hasClienStaked, "Client has not staked");
+    const clients = (await this.clients.get(taskId)).value;
+    assert(Bool.fromValue(clients.length < 3), "Only 3 clients can be added to a task");
+    clients.push(client);
+    await this.clients.set(taskId, clients);
+  }
+
+  @runtimeMethod()
+  public async getTask(taskId: UInt32): Promise<Task> {
+    return (await this.tasks.get(taskId)).value;
+  }
 
 }
--- packages/chain/test/runtime/modules/publish.test.ts
+++ packages/chain/test/runtime/modules/publish.test.ts
@@ -2,48 +2,49 @@ import { TestingAppChain } from "@proto-kit/sdk";
 import { Bool, Bytes, method, PrivateKey, UInt32, UInt64 } from "o1js";
 import { Publisher, Task } from "../../../src/runtime/modules/publish";
 import { log } from "@proto-kit/common";
+import { hasUncaughtExceptionCaptureCallback } from "process";
 
 log.setLevel("ERROR");
 
 describe("balances", () => {
-  it("add task", async () => {
-    const appChain = TestingAppChain.fromRuntime({
-      Publisher,
-    });
+  let appChain = TestingAppChain.fromRuntime({
+    Publisher,
+  })
+  const alicePrivateKey = PrivateKey.random();
+  const alice = alicePrivateKey.toPublicKey();
+  let publisher: Publisher
 
+  const task = Task.from(
+    UInt64.from(1),
+    UInt64.from(1),
+    UInt64.from(1),
+    UInt64.from(1),
+    UInt64.from(1),
+    UInt64.from(1),
+    Bool.fromValue(false),
+    alice,
+    UInt64.from(1)
+  )
+
+
+  beforeAll(async () => {
+    appChain = TestingAppChain.fromRuntime({
+      Publisher
+    });
     appChain.configurePartial({
       Runtime: {
         Publisher: {},
         Balances: {}
       },
     });
-
     await appChain.start();
-
-    const alicePrivateKey = PrivateKey.random();
-    const alice = alicePrivateKey.toPublicKey();
-    const task = Task.from(
-      Bytes.fromHex("Task"),
-      UInt64.from(1),
-      UInt64.from(1),
-      UInt64.from(1),
-      UInt64.from(1),
-      UInt64.from(1),
-      UInt64.from(1),
-      Bool.fromValue(false),
-      alice,
-      UInt64.from(1)
-    )
-
-
-
     appChain.setSigner(alicePrivateKey);
+    publisher = appChain.runtime.resolve("Publisher")
 
-    const publisher = appChain.runtime.resolve("Publisher");
-
+  }, 1_000_000)
+  it("add task", async () => {
     const tx1 = await appChain.transaction(alice, async () => {
-      publisher.addTask(
-        Bytes.fromHex("Task"),
+      await publisher.addTask(
         UInt64.from(1),
         UInt64.from(1),
         UInt64.from(1),
@@ -57,9 +58,14 @@ describe("balances", () => {
     await tx1.send()
 
     const block = await appChain.produceBlock();
+    expect(block?.transactions[0].status.toBoolean()).toBe(true)
     const queriedTask = await appChain.query.runtime.Publisher.tasks.get(UInt32.from(0));
-    expect(queriedTask).toBe(task)
-
+    expect(queriedTask).toStrictEqual(task);
   }, 1_000_000);
+
+  it("complete task", async () => {
+
+
+  })
 });
 
